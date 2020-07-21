ce95a523537e6046cc5d6b319a40afd850aff509
mayacodes <luetke@iu.edu>
2020-07-21 15:36:45 -0700

    adding revised code for tokenizing text, a notebook which creates a new tokenized df on tapas, and gitignore

--- src/preprocessing/make_tokens_column.py
+++ src/preprocessing/make_tokens_column.py
@@ -1,13 +1,18 @@
 # Tokenize module
 
 from nltk.tokenize import RegexpTokenizer
+import pandas as pd
 
 def tokenize_by_column(dataframe,column):
     """
         input: text from a particular column in dataframe
         
-        return: dataframe with new column of tokenized text extracted from the input column 
+        returns: new dataframe with old columns plus the new column of tokenized text extracted from the input column 
     """
     tokenizer = RegexpTokenizer(r'\w+')
-    dataframe['tokenized'+'_'+column] = [tokenizer.tokenize(str(col)) for col in dataframe[column]]
-    return
+    new_col = [tokenizer.tokenize(str(col)) for col in dataframe[column]]
+    df_new_col = pd.DataFrame({'tokenized_' + column: new_col})
+    new_df = dataframe.join(df_new_col)
+    return new_df
+
+
