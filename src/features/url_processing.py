5ff86d02495aff9745002eb68f6fc873471e6bcc
khusakuofm <khusak@umich.edu>
2020-08-20 14:25:11 -0700

    added doc strings to url processing module and removed url replacement function

--- src/features/url_processing.py
+++ src/features/url_processing.py
@@ -2,7 +2,18 @@ import re
 import pandas as pd
 
 def find_url(text):
-    '''
+    '''Finds URLS embedded in a text and extracts them as a list
+    
+    Parameters
+    ----------
+    str: 
+        text
+        
+    Returns
+    -------
+    urls: list
+          
+
     Returns a list of all URLS embedded in a text 
     
     '''
@@ -13,36 +24,56 @@ def find_url(text):
 
     
 def url_list(all_data):
-    '''
-    identifies urls in each article text and creates a new column with 
+    '''Identifies urls in each article text and creates a new column with 
     the list of identified urls as well as 
+    
+    Parameters
+    ----------
+    object: 
+        all_data datarame
+        
+    Returns
+    -------
+    None
+
     '''
     all_data['urls'] = [find_url(cell) for cell in all_data['article_text']]
     return 
 
-def replace_url(all_data):
-    '''
-    Finds and replaces url with 'EMBD_URL' within article text
-    '''
-    pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    all_data['article_text'].replace(to_replace = pattern, value = 'EMBD_HTML', regex=True, inplace=True)
-    return
 
 def url_len(all_data): 
-    '''
-    counts embedded urls for a given article
-    and records the number in a new column
+    '''Counts embedded urls for a given article and records the number in a new column
+    
+    Parameters
+    ----------
+    object: 
+        all_data dataframe
+        
+    Returns
+    -------
+    None 
+    
     '''
     all_data['no_urls'] = [len(x) for x in all_data['urls']]
     return 
 
 def process_urls(all_data):
-    '''
-    Pulling everything together
-    and returns a processed dataframe
+    '''Finds and counts all URLS in a text saving results to 
+    separate columns
+    
+    Parameters
+    ----------
+    object:
+        all_data dataframe
+        
+    Returns
+    -------
+    object:
+        dataframe with two new columns: count of urls in article text,
+        list of all extracted urls
+    
     '''
     url_list(all_data)
     url_len(all_data)
-    replace_url(all_data)
     return all_data
 
