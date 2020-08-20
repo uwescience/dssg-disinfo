66123c92c4af8434d2d2eed0c63b854fafe564fd
Maya <60471986+mayacodes@users.noreply.github.com>
2020-08-20 16:12:46 -0700

    Update model_arch.py

--- models/model_arch.py
+++ models/model_arch.py
@@ -31,23 +31,36 @@ load_dotenv(env_path, override=True)
 _model_arch_registry = {}
 
 def register_model_arch(arch_name, create_fn, param_names):
-    """
-    register_model_arch(name, fn, param_names) registers a model architecture
+    '''
+    register_model_arch(arch_name, create_fn, param_names) registers a model architecture
     with the given name so that it can be built with the build_model() function.
-
-    The name should be a string that identifies the model.
-    The fn should be a function that accepts the parameters in the param_names and
+    
+    Inputs:
+    arch_name: should be a string that identifies the model.
+    create_fn: should be a function that accepts the parameters in the param_names and
         yields a sequential model.
-    The param_names should be a list of the names of the parameters that fn requires.
-    """
+    param_names: should be a list of the names of the parameters that create_fn() requires.
+    
+    Returns:
+    none but does register the model create_fun() and param_names (parameters required for this function
+    in the model registry under the model's name.
+    '''
     # Save the model
     _model_arch_registry[arch_name] = (create_fn, param_names)
 
 def build_model_arch(arch_name, param_dict):
-    """
-    Builds a model using the given architecture name and the given parameter
-    dictionary.
-    """
+    '''
+    build_model_arch(arch_name, param_dict) pulls the model details for the given model
+    from the model registry so that it can be built with the build_model() function.
+    
+    Inputs:
+    arch_name: should be a string that identifies the model.
+    param_dict: a list of parameters that are necessary to run the model.
+    
+    Returns:
+    create function for given model and set of parameters.
+    '''
+    
     # lookup the params and create function:
     (create_fn, params) = _model_arch_registry[arch_name]
     # The f(*[...]) syntax means that instead of being called as f([a,b,c]) the
